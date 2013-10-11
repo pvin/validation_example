4cbcd393afbbd8a5c2191383805ceae7b9c8a763
azri <azri@azri-System-Product-Name.(none)>
2013-10-11 19:12:38 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ValidationExample::Application.config.secret_token = 'c582e42ee7ceb434b8ef0468eab351758ea9a9185fd5035bfa152817a5e7190afe642b687d5d14d23d2ee5482bd04d2fb4d1e340d3d9ad8f2d386b50c1a9ce90'
